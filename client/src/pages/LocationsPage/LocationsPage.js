42aed74dcb4e8c0fb2ab906ca76453b2d26e3488
tmouhamed <s.toqaa@gmail.com>
2019-11-18 17:34:15 -0500

    add more pages

--- client/src/pages/LocationsPage/LocationsPage.js
+++ client/src/pages/LocationsPage/LocationsPage.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class LocationsPage extends React.Component {
     render(){
-        return(null)
+        return(
+            <section className="main__content">
+                <h1 className="main__content-title"> Locations </h1>
+                <input className="main__content-searchInput" type="text" name="search" placeholder="Search" />
+            </section>
+        )
     }
 }
 export default LocationsPage;
