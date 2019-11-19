2d687f6e5bf984c7ce2f2fe1f6ec33102da3a365
tmouhamed <s.toqaa@gmail.com>
2019-11-19 16:32:38 -0500

    add specific warehouse page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import LocationsPage from './pages/LocationsPage/LocationsPage';
 import Nav from './components/Nav/Nav';
+import SpecificWarehousePage from './pages/SpecificWarehousePage/SpecificWarehousePage';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Switch>
-          <Route path="/" exact component={LocationsPage}></Route>
+          <Route path="/" exact component={SpecificWarehousePage}></Route>
           <Route path="/warehouses" exact component={LocationsPage}></Route>
         </Switch>
       </BrowserRouter>
