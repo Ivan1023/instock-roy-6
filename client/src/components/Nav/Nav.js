ca2cfaa5a9eab72436fc7f2daf88b6d4035f4d19
Victor Marage <victor.marage666@gmail.com>
2019-11-19 15:10:27 -0500

    added nav back with proper casing

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.scss'
+import logo from '../../assets/Logo/Logo-instock.svg'
+
+
+
+export default class Nav extends React.Component {
+    render() {
+        return (
+            <header className="nav">
+                <div className="nav__logo-container">
+                    <img className="nav__logo" src={logo} alt="instock logo"></img>
+                </div>
+                <div className="nav__links-container">
+                    <Link className="nav__inventory-link nav__active-link">Inventory</Link>
+                    <Link to="/" className="nav__location-link">Locations</Link>
+                </div>
+            </header>
+        )
+    }
+}
