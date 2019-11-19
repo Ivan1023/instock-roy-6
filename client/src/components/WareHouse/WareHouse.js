5dd37a706a12c0ded5a49c7fe314a2525b2b5097
tmouhamed <s.toqaa@gmail.com>
2019-11-19 13:25:18 -0500

    add heading for desktop view

--- client/src/components/WareHouse/WareHouse.js
+++ client/src/components/WareHouse/WareHouse.js
@@ -6,50 +6,40 @@ class WareHouse extends React.Component {
         return (
             <>
                 <article className="main__content-list">
-                    <div className="main__content-list__row">
-                        <div className="main__content-list__address">
-                            <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
-                            <span>469 King St W, Toronto, ON</span>
+                    <div className="main__content-list__titles">
+                        <div className="main__content-list__titles-firstTitle">
+                            <span>WAREHOUSE</span>
                         </div>
-                        <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
-                    </div>
-                    <div className="main__content-list__row">
-                        <div className="main__content-list__address">
-                            <span>Mara Weinberg</span>
-                            <span>Warehouse Manager</span>
-                        </div>
-                        <div className="main__content-list__address">
-                            <span>+1 416 678 2345</span>
-                            <span>weinberg@instack.com</span>
-                        </div>
-                        <div className="main__content-list__address">
-                            <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
+                        <div className="main__content-list__titles-secondTitle">
+                            <span>CONTACT</span>
+                            <span>CONTACT INFORMATION</span>
+                            <span>CATEGORIES</span>
+                            <span></span>
                         </div>
                     </div>
-                    <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
-                </article>
-                <article className="main__content-list">
-                    <div className="main__content-list__row">
-                        <div className="main__content-list__address">
-                            <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
-                            <span>469 King St W, Toronto, ON</span>
+                    <div className="main__content-list__table">
+                        <div className="main__content-list__row">
+                            <div className="main__content-list__address">
+                                <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
+                                <span>469 King St W, Toronto, ON</span>
+                            </div>
+                            <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
                         </div>
-                        <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
+                        <div className="main__content-list__row">
+                            <div className="main__content-list__address">
+                                <span>Mara Weinberg</span>
+                                <span>Warehouse Manager</span>
+                            </div>
+                            <div className="main__content-list__address">
+                                <span>+1 416 678 2345</span>
+                                <span>weinberg@instack.com</span>
+                            </div>
+                            <div className="main__content-list__address">
+                                <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
+                            </div>
+                        </div>
+                        <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
                     </div>
-                    <div className="main__content-list__row">
-                        <div className="main__content-list__address">
-                            <span>Mara Weinberg</span>
-                            <span>Warehouse Manager</span>
-                        </div>
-                        <div className="main__content-list__address">
-                            <span>+1 416 678 2345</span>
-                            <span>weinberg@instack.com</span>
-                        </div>
-                        <div className="main__content-list__address">
-                            <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
-                        </div>
-                    </div>
-                    <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
                 </article>
             </>
         )
