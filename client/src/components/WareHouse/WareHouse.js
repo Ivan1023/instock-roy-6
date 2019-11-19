637dcf194b5a29b7adc7e10527ce87c2ab4fea64
tmouhamed <s.toqaa@gmail.com>
2019-11-18 22:24:11 -0500

    warehouse item was added as an example

--- client/src/components/WareHouse/WareHouse.js
+++ client/src/components/WareHouse/WareHouse.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import './WareHouse.scss';
 
 class WareHouse extends React.Component {
-    render(){
-        return(null)
+    render() {
+        return (
+            <article className="main__content-list">
+                <div className="main__content-list__address">
+                    <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
+                    <span>469 King St W, Toronto, ON</span>
+                </div>
+                <div className="main__content-list__address">
+                    <span>Mara Weinberg</span>
+                    <span>Warehouse Manager</span>
+                </div>
+                <div className="main__content-list__address">
+                    <span>+1 416 678 2345</span>
+                    <span>weinberg@instack.com</span>
+                </div>
+                <div className="main__content-list__address">
+                    <span>Industrial, Automotive, Heavy, Mechanical,</span>
+                    <span>Engineering, Transportation</span>
+                </div>
+            </article>
+        )
     }
 }
 export default WareHouse;
--- client/src/pages/LocationsPage/LocationsPage.js
+++ client/src/pages/LocationsPage/LocationsPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import './LocationsPage.scss'
+import './LocationsPage.scss';
+import WareHouse from '../../components/WareHouse/WareHouse';
 
 class LocationsPage extends React.Component {
     render() {
         return (
             <section className="main__content">
-                <div className="main__content-header">
+                <article className="main__content-header">
                     <h1 className="main__content-header__title"> Locations </h1>
                     <input className="main__content-header__searchInput" type="text" name="search" placeholder="Search" />
-                </div>
+                </article>
+                <WareHouse />
             </section>
         )
     }
