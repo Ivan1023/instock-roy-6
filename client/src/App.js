acd0644eebfd8c7dd0b3d8db1f8762e4cd404297
tmouhamed <s.toqaa@gmail.com>
2019-11-18 21:45:33 -0500

    add route for warehouse page to APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from './components/Main/Main';
+import LocationsPage from './pages/LocationsPage/LocationsPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Main}></Route>
+          <Route path="/warehouses" exact component={LocationsPage}></Route>
         </Switch>
       </BrowserRouter>
       ,document.getElementById('root')
