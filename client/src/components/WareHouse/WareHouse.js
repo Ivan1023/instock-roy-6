b4749b7a3891d2fda337c1675a712aef84375ae9
tmouhamed <s.toqaa@gmail.com>
2019-11-19 11:10:14 -0500

    add more styling for the location page

--- client/src/components/WareHouse/WareHouse.js
+++ client/src/components/WareHouse/WareHouse.js
@@ -4,29 +4,54 @@ import './WareHouse.scss';
 class WareHouse extends React.Component {
     render() {
         return (
-            <article className="main__content-list">
-                <div className="main__content-list__row">
-                    <div className="main__content-list__address">
-                        <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
-                        <span>469 King St W, Toronto, ON</span>
+            <>
+                <article className="main__content-list">
+                    <div className="main__content-list__row">
+                        <div className="main__content-list__address">
+                            <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
+                            <span>469 King St W, Toronto, ON</span>
+                        </div>
+                        <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
                     </div>
-                    <div className="main__content-list__tabletIcon">
+                    <div className="main__content-list__row">
+                        <div className="main__content-list__address">
+                            <span>Mara Weinberg</span>
+                            <span>Warehouse Manager</span>
+                        </div>
+                        <div className="main__content-list__address">
+                            <span>+1 416 678 2345</span>
+                            <span>weinberg@instack.com</span>
+                        </div>
+                        <div className="main__content-list__address">
+                            <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
+                        </div>
                     </div>
-                </div>
-                <div className="main__content-list__row">
-                    <div className="main__content-list__address">
-                        <span>Mara Weinberg</span>
-                        <span>Warehouse Manager</span>
+                    <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
+                </article>
+                <article className="main__content-list">
+                    <div className="main__content-list__row">
+                        <div className="main__content-list__address">
+                            <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
+                            <span>469 King St W, Toronto, ON</span>
+                        </div>
+                        <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
                     </div>
-                    <div className="main__content-list__address">
-                        <span>+1 416 678 2345</span>
-                        <span>weinberg@instack.com</span>
+                    <div className="main__content-list__row">
+                        <div className="main__content-list__address">
+                            <span>Mara Weinberg</span>
+                            <span>Warehouse Manager</span>
+                        </div>
+                        <div className="main__content-list__address">
+                            <span>+1 416 678 2345</span>
+                            <span>weinberg@instack.com</span>
+                        </div>
+                        <div className="main__content-list__address">
+                            <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
+                        </div>
                     </div>
-                    <div className="main__content-list__address">
-                        <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
-                    </div>
-                </div>
-            </article>
+                    <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
+                </article>
+            </>
         )
     }
 }
