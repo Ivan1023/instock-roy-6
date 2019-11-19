0ad6729417d137af4a51305e8391db0e41eb70a3
Eric Thorfinnson <ethorf@gmail.com>
2019-11-19 10:19:29 -0500

    started nav bar tuesday

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from '../nav/nav.js'
 
 class Main extends React.Component {
     render() {
 
         return(
             <div>
+                <Nav />
                 <p>Hello World</p>
             </div>
         )
