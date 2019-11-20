ad40dd372594705bacf716873cbed2e29044674f
tmouhamed <s.toqaa@gmail.com>
2019-11-19 23:14:33 -0500

    add more styling

--- client/src/components/InventoryProducts/InventoryProducts.js
+++ client/src/components/InventoryProducts/InventoryProducts.js
@@ -7,11 +7,21 @@ class InventoryProducts extends React.Component {
         return (
             <article className="main__list">
                 <div className="main__list-titles">
-                    <span>Item</span>
-                    <span>LAST ORDERED</span>
-                    <span>LOCATION</span>
-                    <span>QUANTITY</span>
-                    <span>STATUS</span>
+                    <div className="main__list-titles__head">
+                        <span>Item</span>
+                    </div>
+                    <div className="main__list-titles__head">
+                        <span>Last Ordered</span>
+                    </div>
+                    <div className="main__list-titles__head">
+                        <span>Location</span>
+                    </div>
+                    <div className="main__list-titles__head">
+                        <span>Quantity</span>
+                    </div>
+                    <div className="main__list-titles__head">
+                        <span>Status</span>
+                    </div>
                 </div>
                 <Link to="/warehouses" className="main__list-items">
                     <div className="main__list-items__group">
@@ -39,6 +49,33 @@ class InventoryProducts extends React.Component {
                     </div>
                     <div className="main__list-items__icon"></div>
                 </Link>
+                <Link to="/warehouses" className="main__list-items">
+                    <div className="main__list-items__group">
+                        <h4 className="main__list-items__group-title">Item</h4>
+                    </div>
+                    <div className="main__list-items__group">
+                        <h3>Product Name Here</h3>
+                        <span>Here is a very brief description of the product in the inventory...</span>
+                    </div>
+                    <div className="main__list-items__group">
+                        <h4 className="main__list-items__group-title">LAST ORDERED</h4>
+                        <span>05/24/2018</span>
+                    </div>
+                    <div className="main__list-items__group">
+                        <h4 className="main__list-items__group-title">LOCATION</h4>
+                        <span>Toronto, CAN</span>
+                    </div>
+                    <div className="main__list-items__group">
+                        <h4 className="main__list-items__group-title">QUANTITY</h4>
+                        <span>12,000</span>
+                    </div>
+                    <div className="main__list-items__group">
+                        <h4 className="main__list-items__group-title">STATUS</h4>
+                        <span>In Stock</span>
+                    </div>
+                    <div className="main__list-items__icon"></div>
+                </Link>
+
             </article>
         )
     }
