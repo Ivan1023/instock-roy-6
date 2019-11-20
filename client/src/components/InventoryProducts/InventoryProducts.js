4328303b977bebed2b32c7c26851b5883ac4d571
tmouhamed <s.toqaa@gmail.com>
2019-11-20 14:14:38 -0500

    add a warehouse products styling

--- client/src/components/InventoryProducts/InventoryProducts.js
+++ client/src/components/InventoryProducts/InventoryProducts.js
@@ -1,82 +1,37 @@
 import React from 'react';
 import './InventoryProducts.scss';
-import { Link } from 'react-router-dom';
 
 class InventoryProducts extends React.Component {
     render() {
         return (
-            <article className="main__list">
-                <div className="main__list-titles">
-                    <div className="main__list-titles__head">
-                        <span>Item</span>
-                    </div>
-                    <div className="main__list-titles__head">
-                        <span>Last Ordered</span>
-                    </div>
-                    <div className="main__list-titles__head">
-                        <span>Location</span>
-                    </div>
-                    <div className="main__list-titles__head">
-                        <span>Quantity</span>
-                    </div>
-                    <div className="main__list-titles__head">
-                        <span>Status</span>
-                    </div>
-                </div>
-                <Link to="/warehouses" className="main__list-items">
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">Item</h4>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h3>Product Name Here</h3>
-                        <span>Here is a very brief description of the product in the inventory...</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">LAST ORDERED</h4>
-                        <span>05/24/2018</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">LOCATION</h4>
-                        <span>Toronto, CAN</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">QUANTITY</h4>
-                        <span>12,000</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">STATUS</h4>
-                        <span>In Stock</span>
-                    </div>
-                    <div className="main__list-items__icon"></div>
-                </Link>
-                <Link to="/warehouses" className="main__list-items">
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">Item</h4>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h3>Product Name Here</h3>
-                        <span>Here is a very brief description of the product in the inventory...</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">LAST ORDERED</h4>
-                        <span>05/24/2018</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">LOCATION</h4>
-                        <span>Toronto, CAN</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">QUANTITY</h4>
-                        <span>12,000</span>
-                    </div>
-                    <div className="main__list-items__group">
-                        <h4 className="main__list-items__group-title">STATUS</h4>
-                        <span>In Stock</span>
-                    </div>
-                    <div className="main__list-items__icon"></div>
-                </Link>
-
-            </article>
+            < article className="main" >
+                <table className="main__table">
+                    <thead className="main__table-titles">
+                        <tr className="main__table-titles-row">
+                            <th>item</th>
+                            <th>last ordered</th>
+                            <th>location</th>
+                            <th>quantity</th>
+                            <th>status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody className="main__table-body">
+                        <tr className="main__table-body__head">
+                            <td className="main__table-body__data" data-label="Item">
+                                <h3>Product Name Here</h3>Here is a very brief description of the product in the inventory...
+                            </td>
+                            <td className="main__table-body__data"></td>
+                            <td className="main__table-body__data" data-label="Last Ordered">05/24/2018</td>
+                            <td className="main__table-body__data" data-label="Location">Toronto, CAN</td>
+                            <td className="main__table-body__data" data-label="Quantity">12,000</td>
+                            <td className="main__table-body__data" data-label="Status">In Stock</td>
+                            <td className="main__table-body__data"><img alt="3 dots" className="main__table-body__data-input"/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </article >
         )
     }
 }
