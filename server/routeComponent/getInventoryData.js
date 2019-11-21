5dcb42ae9047b42f9ca9cf79f89572f26c759bd4
victor <victor.marage666@gmail.com>
2019-11-21 16:45:33 -0500

    Back-End: POST Inventory Item

--- server/routeComponent/getInventoryData.js
+++ server/routeComponent/getInventoryData.js
@@ -2,10 +2,21 @@ const express = require ('express');
 const router = express.Router();
 const invData = require('../data/inventory.json')
 
-router.get('/', (request, response)=>{
+router.get('/', (request, response) => {
     
     response.send(invData)
     
+})
+
+router.post('/', (req, res) => {
+    // console.log(req.body)
+
+    if(req.body!== undefined) {
+        invData.push(req.body)
+    }
+    else{console.log('400 status')}
+
+    res.send(invData);
 })
 
 module.exports = router;
