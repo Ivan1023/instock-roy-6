03ee64d2fe8302758d9ad394918bfdf4dc24df78
tmouhamed <s.toqaa@gmail.com>
2019-11-21 16:13:39 -0500

    everything is there

--- client/src/components/InventoryProducts/InventoryProducts.js
+++ client/src/components/InventoryProducts/InventoryProducts.js
@@ -16,23 +16,23 @@ class InventoryProducts extends React.Component {
                             <th></th>
                         </tr>
                     </thead>
-                    {this.props.products.map((item) => {
-                        return(
-                            <tbody className="main__table-body" key={item.id}>
-                            <tr className="main__table-body__head">
-                                <td className="main__table-body__data">
-                                    <p className="main__table-body__data-item">Item</p>
-                        <h3 className="main__table-body__data-firstTitle">{item.name}</h3>{item.description}
-                                </td>
-                                <td className="main__table-body__data" data-label="Last Ordered">{item.lastOrdered}</td>
-                                <td className="main__table-body__data" data-label="Location">{item.location}</td>
-                                <td className="main__table-body__data" data-label="Quantity">{item.quantity}</td>
-                                <td className="main__table-body__data" data-label="Status">{item.isInstock ? "In Stock" : "Out of Stock"}</td>
-                                <td className="main__table-body__data"><img alt="3 dots" className="main__table-body__data-input"/></td>
-                            </tr>
-                        </tbody>
-                        );
-                    })}
+                    <tbody className="main__table-body">
+                        {this.props.products.map((item) => {
+                            return (
+                                <tr className="main__table-body__head" key={item.id}>
+                                    <td className="main__table-body__data">
+                                        <p className="main__table-body__data-item">Item</p>
+                                        <h3 className="main__table-body__data-firstTitle">{item.name}</h3>{item.description}
+                                    </td>
+                                    <td className="main__table-body__data" data-label="Last Ordered">{item.lastOrdered}</td>
+                                    <td className="main__table-body__data" data-label="Location">{item.location}</td>
+                                    <td className="main__table-body__data" data-label="Quantity">{item.quantity}</td>
+                                    <td className="main__table-body__data" data-label="Status">{item.isInstock ? "In Stock" : "Out of Stock"}</td>
+                                    <td className="main__table-body__data"><img alt="3 dots" className="main__table-body__data-input" /></td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
                 </table>
             </article >
         )
