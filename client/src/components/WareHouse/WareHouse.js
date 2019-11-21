45200b9e82cd15fa29640cb1a99f6ec8e529e566
tmouhamed <s.toqaa@gmail.com>
2019-11-21 16:02:41 -0500

    warehouses information on the screen

--- client/src/components/WareHouse/WareHouse.js
+++ client/src/components/WareHouse/WareHouse.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 
 class WareHouse extends React.Component {
     render() {
-        console.log(this.props)
         return (
             <>
                 <article className="main__content-list">
@@ -20,27 +19,26 @@ class WareHouse extends React.Component {
                         </div>
                     </div>
                     {this.props.warehouses.map((item) => {
-                        console.log(item);
                         return (
-                            <Link to="/warehouses/warehouse" className="main__content-list__table">
+                            <Link to="/warehouses/warehouse" className="main__content-list__table" key={item.id}>
                                 <div className="main__content-list__row">
                                     <div className="main__content-list__address">
-                                        <h2 className="main__content-list__address-title">Warehouse Number 1</h2>
-                                        <span>469 King St W, Toronto, ON</span>
+                                        <h2 className="main__content-list__address-title">{item.name}</h2>
+                                        <span>{item.address.street}</span>
                                     </div>
                                     <div className="main__content-list__arrowIcon main__content-list__tabletIcon"></div>
                                 </div>
                                 <div className="main__content-list__row">
                                     <div className="main__content-list__address">
-                                        <span>Mara Weinberg</span>
-                                        <span>Warehouse Manager</span>
+                                        <span>{item.contact.name}</span>
+                                        <span>{item.contact.title}</span>
                                     </div>
                                     <div className="main__content-list__address">
-                                        <span>+1 416 678 2345</span>
-                                        <span>weinberg@instack.com</span>
+                                        <span>{item.contact.phone}</span>
+                                        <span>{item.contact.email}</span>
                                     </div>
                                     <div className="main__content-list__address">
-                                        <span>Industrial, Automotive, Heavy, Mechanical, Engineering</span>
+                                        <span>{item.inventoryCategories}</span>
                                     </div>
                                 </div>
                                 <div className="main__content-list__arrowIcon main__content-list__desktopIcon"> </div>
