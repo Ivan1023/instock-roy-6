06da82bc30ab3a138728ed34850e4699b87d5ef8
victor <victor.marage666@gmail.com>
2019-11-21 15:33:07 -0500

    creation of productpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Inventory from './pages/Inventory/Inventory'
+// import Inventory from './pages/Inventory/Inventory';
 import LocationsPage from './pages/LocationsPage/LocationsPage';
 import InventoryPage from './pages/InventoryPage/InventoryPage';
 import Nav from './components/Nav/Nav';
 import SpecificWarehousePage from './pages/SpecificWarehousePage/SpecificWarehousePage';
+// import InventoryProducts from './components/Products/Products';
+import ProductPage from './pages/ProductPage/ProductPage';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <Route path="/" exact component={InventoryPage}></Route>
           <Route path="/warehouses" exact component={LocationsPage}></Route>
           <Route path="/warehouses/warehouse" component={SpecificWarehousePage}></Route>
+          {/* <Route path="/inventoryproducts" component={InventoryProducts}></Route> */}
+          <Route path="/products" exact component={ProductPage}></Route>
         </Switch>
       </BrowserRouter>
       ,document.getElementById('root')
--- client/src/pages/ProductPage/ProductPage.js
+++ client/src/pages/ProductPage/ProductPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './InventoryProduct.scss';
+import './ProductPage.scss';
 import { Link } from 'react-router-dom';
 
 class InventoryProduct extends React.Component {
@@ -11,20 +11,20 @@ class InventoryProduct extends React.Component {
                 <main>
                     <section className="product__content">
                         <article className="product__content-box">
-                        <Link to="/inventory"><div className="product__content-icon"></div></Link>
+                        <Link to="/"><div className="product__content-icon"></div></Link>
                             <h1 className="product__content-box__title">Product Name</h1>
                         </article>
                         <div class="product__content-box__instock">In Stock</div>
                     </section>
 
                     <div className="product__content-description">
-                        <div>
+                        <div className="product__content-description-title">
                             <span>ITEM DESCRIPTIONS</span>
                             <p className="product__content-description__p">Here is a more detailed summary of the product name, it’s uses, industries and possible attributes that could be used to describe the product</p>
                         </div>
 
-                        <div>
-                            {/* <div className=""> */}
+                        <div className="box1">
+                            <div className="box1__content">
                                 <div className="product__content-description__items">
                                     <span className="product__content-description__titles">ORDERED BY</span>
                                     <p className="product__content-description__info">Mark Saunders</p>
@@ -33,26 +33,33 @@ class InventoryProduct extends React.Component {
                                     <span className="product__content-description__titles">REFERENCE NUMBER</span>
                                     <p className="product__content-description__info">JK2020OISFBIO</p>
                                 </div>
-                            {/* </div> */}
-                            <div className="product__content-description__items">
-                                <span className="product__content-description__titles">LAST ORDERED</span>
-                                <p className="product__content-description__info">5/24/18</p>
                             </div>
-                            <div className="product__content-description__items">
-                                <span className="product__content-description__titles">LOCATION</span>
-                                <p className="product__content-description__info">Toronto, CAN</p>
+
+                            <div className="box1__content">
+                                <div className="product__content-description__items">
+                                    <span className="product__content-description__titles">LAST ORDERED</span>
+                                    <p className="product__content-description__info">5/24/18</p>
+                                </div>
+                                <div className="product__content-description__items">
+                                    <span className="product__content-description__titles">LOCATION</span>
+                                    <p className="product__content-description__info">Toronto, CAN</p>
+                                </div>
                             </div>
+
                             <div className="product__content-description__items">
-                                <span className="product__content-description__titles">QUANTITY</span>
-                                <p className="product__content-description__info">120000</p>
-                            </div>
-                            <div className="product__content-description__items">
-                                <span className="product__content-description__titles">CATEGORIES</span>
-                                <p className="product__content-description__info">Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation, Sales</p>
-                            </div>
+                        <span className="product__content-description__titles">QUANTITY</span>
+                        <p className="product__content-description__info">120000</p>
+                    </div>
+                    <div className="product__content-description__items">
+                        <span className="product__content-description__titles">CATEGORIES</span>
+                        <p className="product__content-description__info">Industrial, Automotive, Heavy, Mechanical, Engineering, Transportation, Sales</p>
+                    </div>
                         </div>
                     </div>
-                    <button className="product__content-button" type="button">EDIT</button>
+
+                    
+
+                <button className="product__content-button" type="button">EDIT</button>
 
                 </main>
             </>
