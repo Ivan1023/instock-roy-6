a8a65c8aecdac7f108789e3e1a44f2fa3e15aa43
Ivan Chau <ivan.chau1023@gmail.com>
2019-11-21 22:25:25 -0500

    fixed remove button

---
+++ client/src/components/RemoveButton/RemoveButton.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class RemoveButton extends React.Component{
+    container = React.createRef();
+    state = {
+        open: false,
+    };
+
+    handleButtonClick = () => {
+        this.setState(state => {
+            return {
+                open: !state.open,
+            };
+        });
+    };
+
+    handleClickOutside = event => {
+        if (this.container.current && !this.container.current.contains(event.target)) {
+            this.setState({
+                open: false,
+            });
+        }
+    };
+
+    componentDidMount() {
+        document.addEventListener("mousedown", this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("mousedown", this.handleClickOutside);
+    }
+
+    render () {
+    
+        return (
+            <div className="dropdown" ref={this.container}>
+                <img onClick={this.handleButtonClick} alt="3 dots" className="main__table-body__data-input" />
+                {this.state.open && (<button className="dropdown__position">Remove</button>)}
+            </div>
+        )
+    }
+}
+
+export default RemoveButton
